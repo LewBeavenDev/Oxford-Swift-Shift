79dc5094473a74b8022ebf9d9a4109587f0d346f
LewBeavenDev <elu95@hotmail.co.uk>
2024-11-07 16:27:09 +0000

    add footer contact number and email

--- Pristine Removals/components/footer.tsx
+++ Pristine Removals/components/footer.tsx
@@ -1,14 +1,31 @@
 "use client";
 import { NextUIProvider, Divider } from "@nextui-org/react";
 import React from "react";
+import Image from "next/image"; // Ensure this import matches your usage context
 
 const Footer = () => {
   return (
     <NextUIProvider>
-      <div className="w-full">
+      <div className="flex flex-col w-full">
         <Divider className="w-20 mx-auto my-10 rounded-xl bg-gradient-to-r from-cyan-600 to-gray-500" />
-        <div className="flex justify-center">
-          <h1>Hello Footer</h1>
+        <div className="flex flex-row justify-between h-20">
+          <div className="w-1/2 px-10">
+            <p>
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+                Email: dilwar2008@live.co.uk
+              </span>
+            </p>
+          </div>
+          <div className="w-1/2 px-10 text-end">
+            <p>
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+                Phone: 07887631348
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center pb-10">
+          <Image alt="logo" height={200} src="/vertical-logo.png" width={200} />
         </div>
       </div>
     </NextUIProvider>
