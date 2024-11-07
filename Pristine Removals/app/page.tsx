2940baf60e38d66b55c76c4d00c3fdeb34f7e915
LewBeavenDev <elu95@hotmail.co.uk>
2024-11-07 14:07:40 +0000

    add about section to page.tsx

--- Pristine Removals/app/page.tsx
+++ Pristine Removals/app/page.tsx
@@ -1,13 +1,92 @@
 "use client";
 import * as React from "react";
-import { NextUIProvider, Image } from "@nextui-org/react";
-
+import { NextUIProvider, Image, Divider, Button } from "@nextui-org/react";
 export default function Home() {
   return (
     <NextUIProvider>
-      <Image
-        src="" 
-      />
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center w-3/4 h-1/2">
+          <Image
+            isBlurred
+            isZoomed
+            alt="Removals Van"
+            src="/pristine-removals-van.jpg"
+          />
+          <div className="absolute z-10 w-1/2 p-1 shadow-lg shadow-black/50 rounded-xl bg-slate-600/50">
+            <Image
+              isBlurred
+              alt="Pristine Removals Logo"
+              src="/horizontal-logo.png"
+            />
+          </div>
+        </div>
+      </div>
+      <Divider className="w-20 mx-auto my-10 rounded-xl bg-gradient-to-r from-cyan-600 to-gray-500" />
+      <div className="flex justify-start w-full px-32 text-start max-h-min">
+        <p className="w-90% text-6xl text-transparent text-wrap bg-clip-text bg-gradient-to-r from-cyan-600 to-gray-400">
+          Welcome to the Removal Solution of the{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+            Future{" "}
+          </span>
+        </p>
+      </div>
+      <Divider className="w-20 mx-auto my-10 rounded-xl bg-gradient-to-r from-cyan-600 to-gray-500" />
+      <div className="flex justify-between gap-4 px-32 text-start">
+        <div className="">
+          <p className="text-start">
+            Here at{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-gray-400">
+              Pristine Removals
+            </span>
+            , for the past{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+              Ten Years
+            </span>
+            , we are proud to have delivered{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+              Consistent Results
+            </span>
+            , to the{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+              Highest Moving Standards.
+            </span>{" "}
+            From complete house moves to just one item of furniture, we will
+            move it.{" "}
+          </p>
+          <Button className="mt-4" color="primary" size="sm" variant="bordered">
+            {" "}
+            Find out More{" "}
+          </Button>
+          <Divider className="w-20 mx-auto my-4 rounded-xl bg-gradient-to-r from-cyan-600 to-gray-500" />
+          <p className="text-start">
+            Due to our wide array of services, we have pricing to suit all
+            needs. Check out our{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-600 to-red-400">
+              Pricing{" "}
+            </span>
+            via the button below!
+          </p>
+          <Button
+            className="mt-4 border-2 bg-gradient-to-r from-amber-600/15 to-red-400/15 border-amber-600"
+            size="sm"
+            variant="bordered"
+          >
+            {" "}
+            Pricing{" "}
+          </Button>
+        </div>
+        <div className="">
+          <Image
+            isBlurred
+            isZoomed
+            alt="Boxes in House"
+            className="overflow-hidden bg-cover"
+            height={450}
+            src="/moving-1.jpg"
+            width={2000}
+          />
+        </div>
+      </div>
     </NextUIProvider>
   );
 }
--- Pristine Removals/components/navbar.tsx
+++ Pristine Removals/components/navbar.tsx
@@ -7,10 +7,7 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-// import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
@@ -18,47 +15,18 @@ import Image from "next/image";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  // TwitterIcon,
-  // GithubIcon,
-  // DiscordIcon,
-  // HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="flex-shrink-0 text-base pointer-events-none text-default-400" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex items-center justify-start gap-1" href="/">
-            <Image 
-              src="/mobile-logo.png"
+            <Image
               alt="Pristine Removals"
-              width={32}
               height={32}
+              src="/mobile-logo.png"
+              width={32}
             />
             <p className="font-bold text-inherit">Pristine Removals</p>
           </NextLink>
@@ -86,16 +54,7 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden gap-2 sm:flex">
-          {/* <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
-            <TwitterIcon className="text-default-500" />
-          </Link> */}
-          {/* <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
-            <DiscordIcon className="text-default-500" />
-          </Link> */}
-          {/* <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-            <GithubIcon className="text-default-500" />
-          </Link> */}
-          <ThemeSwitch className="hidden lg:flex"/>
+          <ThemeSwitch className="hidden lg:flex" />
         </NavbarItem>
       </NavbarContent>
 
@@ -105,7 +64,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        <div className="flex flex-col gap-2 mx-4 mt-2">
+        <div className="flex flex-col gap-2 mx-4 mt-">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
