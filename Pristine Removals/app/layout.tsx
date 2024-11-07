975a02ee29bc825462c36d3c3a12cc7055c03493
LewBeavenDev <elu95@hotmail.co.uk>
2024-11-07 14:43:36 +0000

    git add initial footer

--- Pristine Removals/app/layout.tsx
+++ Pristine Removals/app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: {
@@ -47,9 +48,7 @@ export default function RootLayout({
             <main className="container flex-grow px-6 pt-16 mx-auto max-w-7xl">
               {children}
             </main>
-            <footer className="flex items-center justify-center w-full py-3">
-              Hello Footer
-            </footer>
+            <Footer />
           </div>
         </Providers>
       </body>
---
+++ Pristine Removals/components/footer.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { NextUIProvider, Divider } from "@nextui-org/react";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <NextUIProvider>
+      <div>
+        <Divider className="w-4/5 mx-auto my-10 rounded-xl bg-gradient-to-r from-cyan-600 to-gray-500" />
+        <div className="flex justify-center">
+          <h1>Hello Footer</h1>
+        </div>
+      </div>
+    </NextUIProvider>
+  );
+};
+
+export default Footer;
