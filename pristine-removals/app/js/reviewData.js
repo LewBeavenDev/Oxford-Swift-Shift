d1e0f7c97c65d5b8474bd42af8e1eb75f634a996
LewBeavenDev <elu95@hotmail.co.uk>
2024-11-08 11:25:50 +0000

    add real reviews

---
+++ pristine-removals/app/js/reviewData.js
@@ -0,0 +1,16 @@
+export const reviews = [
+  {
+    name: "Poppy Gunn",
+review: "Fab service with a smile! The guys were really helpful and picked up a bed frame for me on really short notice! I will definitely be using them again.",
+  },
+  {
+    name: "R. Galagoda",
+    review:
+      "Moving is stressful and Dill and his team made it so much easier. Great service, professional and friendly, put me  at ease, and everything was taken care of! Highly recommend and would use them again!",
+  },
+  {
+    name: "Reya Li",
+    review:
+      "I am very satisfied with the service. From the moment they arrived, it was clear that they were professional and highly skilled. The team worked efficiently and carefully, ensuring that all my belongings were handled with the good care.",
+  },
+];
